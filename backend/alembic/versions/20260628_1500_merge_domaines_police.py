1ab6d3fa7d8ad3e72c1d3f8631f3a992fa73d9be
Nihad ABOUDOU TRAORE <nihad@digiid.app>
2026-06-28 11:34:40 +0000

    fix: merge alembic + correction backref domaine/departement

---
+++ backend/alembic/versions/20260628_1500_merge_domaines_police.py
@@ -0,0 +1,18 @@
+"""merge domaines et police
+
+Revision ID: 20260628_1500
+Revises: 20260628_1000, police_001
+Create Date: 2026-06-28
+"""
+from alembic import op
+
+revision = '20260628_1500'
+down_revision = ('20260628_1000', 'police_001')
+branch_labels = None
+depends_on = None
+
+def upgrade() -> None:
+    pass
+
+def downgrade() -> None:
+    pass
